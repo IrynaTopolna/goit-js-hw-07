e55abeaf20f1f584d5d1f1ead1633fd4b72b7eb9
IrynaTopolna <itopolna2808@gmail.com>
2022-12-19 21:32:10 +0200

    Adds tasks 1-2 JS and HTML script

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+
+const markup = galleryItems.reduce(
+  (acc, { preview, original, description }) =>
+    acc +
+    `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`,
+  ""
+);
+
+galleryRef.insertAdjacentHTML("beforeend", markup);
+
+galleryRef.addEventListener("click", onGalleryImageClick);
+
+function onGalleryImageClick(evt) {
+  evt.preventDefault();
+
+  if (evt.target.classList.contains("gallery__image")) {
+    const imgOriginalSrc = evt.target.dataset.source;
+    console.log(imgOriginalSrc);
+
+    const instance = basicLightbox.create(`
+    <img src='${imgOriginalSrc}',>
+`);
+
+    instance.show();
+  }
+}
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+
+const markup = galleryItems.reduce(
+  (acc, { preview, original, description }) =>
+    acc +
+    `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</div>`,
+  ""
+);
+
+galleryRef.insertAdjacentHTML("beforeend", markup);
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+gallery.on("show.simplelightbox");
